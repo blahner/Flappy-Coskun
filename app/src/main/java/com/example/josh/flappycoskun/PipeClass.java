3034e2a28353e0e7f2a32d33b8fb6d7cfe084fb6
josh-bone <40771004+josh-bone@users.noreply.github.com>
2018-12-08 13:28:41 -0500

    V 1.0
    Pipe-Bird interactions, randomized pipe y position

--- app/src/main/java/com/example/josh/flappycoskun/PipeClass.java
+++ app/src/main/java/com/example/josh/flappycoskun/PipeClass.java
@@ -12,21 +12,36 @@ class PipeClass {
 
     private Bitmap image; //top
     private Bitmap image2; //bottom
-    public int yPos, xPos;
-    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private int yPos, xPos;
+    public static int width = 200;
+    public static int height = Resources.getSystem().getDisplayMetrics().heightPixels; //screenheight (for good measure)
+    public static int gapSpacing = Birdy.height * 3; //space of gap between pipes
 
     public PipeClass (Bitmap bmp, Bitmap bmp2, int x, int y){ //constructor
         image = bmp;
         image2 = bmp2;
-        yPos = y; //top
-        xPos = x; //left
+        resetyPos();//position of top pipe (note the pipes hang off of the top of the screen)
+        xPos = x; //left corner
     }
 
     public void draw(Canvas canvas){
-        canvas.drawBitmap(image, xPos, yPos -(gapHeight / 2), null); //draw the bottom pipe
-        canvas.drawBitmap(image2, xPos, yPos + (GameView.gapHeight / 2) + (screenHeight / 2), null); //draw the top pipe
+        canvas.drawBitmap(image, xPos, yPos, null); //draw the top pipe
+        canvas.drawBitmap(image2, xPos, yPos + height + gapSpacing, null); //draw the bottom pipe
     }
+
     public void update(){
         xPos -= velocity; //move pipes in x-dir with time
+    }
+    public int getxPos(){
+        return xPos;
+    }
+    public int getyPos(){
+        return yPos;
+    }
+    public void resetyPos(){
+        yPos = - height + (int) (Math.random() * (height/2) );
+    }
+    public void setxPos(int x){
+        xPos = x;
     }
 }
